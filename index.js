88d7eb36fec3c4db972e14afa2976a76af641ac4
zhipu77 <rocture@qq.com>
2018-03-12 16:46:54 +0800

    更新写法

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-module.exports = {
-  getHashQuery() {
-    const result = {}
-    const query = window.location.hash.split('?')[1]
-    if (query) {
-      const kvs = query.split('&')
-      kvs.forEach(ele => {
-        let kv = ele.split('=')
-        result[kv[0]] = kv[1]
-      })
-    }
-    return result
-  },
+const getHashQuery = function() {
+  const result = {}
+  const query = window.location.hash.split('?')[1]
+  if (query) {
+    const kvs = query.split('&')
+    kvs.forEach(ele => {
+      let kv = ele.split('=')
+      result[kv[0]] = kv[1]
+    })
+  }
+  return result
+}
 
-  getSearchQuery() {
-    const result = {}
-    const search = window.location.search.replace('?', '')
-    if (search) {
-      const kvs = search.split('&')
-      kvs.forEach(ele => {
-        let kv = ele.split('=')
-        result[kv[0]] = kv[1]
-      })
-    }
-    return result
-  },
+const getSearchQuery = function() {
+  const result = {}
+  const search = window.location.search.replace('?', '')
+  if (search) {
+    const kvs = search.split('&')
+    kvs.forEach(ele => {
+      let kv = ele.split('=')
+      result[kv[0]] = kv[1]
+    })
+  }
+  return result
+}
+
+module.exports = {
+  getHashQuery: getHashQuery(),
+
+  getSearchQuery: getSearchQuery(),
 
   updateSearchQuery(kv) {
     let searchStr = '?';
