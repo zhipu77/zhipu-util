c6dc4ab9626256017683c307c411b8c19b4885b0
zhipu77 <rocture@qq.com>
2018-03-12 16:56:21 +0800

    每次实时获取参数，不需要缓存

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const getHashQuery = function() {
-  const result = {}
-  const query = window.location.hash.split('?')[1]
-  if (query) {
-    const kvs = query.split('&')
-    kvs.forEach(ele => {
-      let kv = ele.split('=')
-      result[kv[0]] = kv[1]
-    })
-  }
-  return result
-}
-
-const getSearchQuery = function() {
-  const result = {}
-  const search = window.location.search.replace('?', '')
-  if (search) {
-    const kvs = search.split('&')
-    kvs.forEach(ele => {
-      let kv = ele.split('=')
-      result[kv[0]] = kv[1]
-    })
-  }
-  return result
-}
-
 module.exports = {
-  getHashQuery: getHashQuery(),
+  getHashQuery() {
+    const result = {}
+    const query = window.location.hash.split('?')[1]
+    if (query) {
+      const kvs = query.split('&')
+      kvs.forEach(ele => {
+        let kv = ele.split('=')
+        result[kv[0]] = kv[1]
+      })
+    }
+    return result
+  },
 
-  getSearchQuery: getSearchQuery(),
-
+  getSearchQuery() {
+    const result = {}
+    const search = window.location.search.replace('?', '')
+    if (search) {
+      const kvs = search.split('&')
+      kvs.forEach(ele => {
+        let kv = ele.split('=')
+        result[kv[0]] = kv[1]
+      })
+    }
+    return result
+  },
+  
   updateSearchQuery(kv) {
     let searchStr = '?';
     const search = window.location.search.replace('?', '')
