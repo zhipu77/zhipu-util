00b6bb1f409926be6d68045ad35c6e64cd754dc3
zhipu77 <rocture@qq.com>
2018-03-12 15:21:17 +0800

    add search query

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ module.exports = {
       })
     }
     return result
+  },
+
+  getSearchQuery() {
+    const result = {}
+    const search = window.location.search.replace('?', '')
+    if (search) {
+      const kvs = search.split('&')
+      kvs.forEach(ele => {
+        let kv = ele.split('=')
+        result[kv[0]] = kv[1]
+      })
+    }
+    return result
   }
 }
